c988a15697de4ec4d54c912906d93e79f08cd84a
JH Baek <zirubak@gmail.com>
2014-06-26 23:49:34 -0700

    HelloWorld example
    자 c++ 다시 시작해 보자..

---
+++ example.cpp
@@ -0,0 +1,23 @@
+/*
+ * example.cpp
+ *
+ *  Created on: Jun 26, 2014
+ *      Author: jh
+ */
+
+
+#include <iostream>
+
+int main(void)
+{
+	int num = 20;
+
+	std::cout<<"Hello World!"<<std::endl;
+	std::cout<<"Hello "<<"World!"<<std::endl;
+	std::cout<<num<<' '<<'A';
+	std::cout<<' '<<3.14<<std::endl;
+	return 0;
+}
+
+
+
