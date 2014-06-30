9623fab5ccae72973c255e376005caf8b7116605
JH Baek <zirubak@gmail.com>
2014-06-29 20:20:24 -0700

    default value 2 page 31
    오른쪽 매개 변수의 디폴트 값을 비우는 형태로는 디폴트 값을 지정할 수가 없다.
    반드시 오른쪽 매개 변수의 디폴트 값 부터 채우는 형태로 정의해야 한다. 
    
    왜냐하면 함수에 전달되는 인자가 왼쪽에서 부터 오른쪽으로 전달 되기 때문이다.

--- example.cpp
+++ example.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 
-
-int Adder(int num1=1, int num2=2);
+int BoxVolume(int lenth, int width = 1, int height = 1);
 
 int main(void)
 {
-	std::cout << Adder() << std::endl;
-	std::cout << Adder(5) << std::endl;
-	std::cout << Adder(3,5) << std::endl;
-
+	std::cout << "[3,3,3]" << BoxVolume(3,3,3) << std::endl;
+	std::cout << "[5,5,D]" << BoxVolume(5,5) << std::endl;
+	std::cout << "[7,D,D]" << BoxVolume(7) << std::endl;
+//	std::cout << "[D,D,D]" << BoxVolume() << std::endl;
 	return 0;
 }
 
-int Adder(int num1, int num2)
+int BoxVolume(int lenth, int width, int height)
 {
-	return num1+num2;
+	return lenth*width*height;
 }
