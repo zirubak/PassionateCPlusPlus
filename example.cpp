8aab30226a1ec6d08fd6f016296aabab40fac0af
JH Baek <zirubak@gmail.com>
2014-06-29 21:00:20 -0700

    name space 2 page 40
    함수는 선언과 정의를 일반적으로 분리한다. 
    '함수의 선언'은 헤더에
    '함수의 정의'는 소스파일에
    
    namespace의 경우는 이렇게 한다.

--- example.cpp
+++ example.cpp
@@ -2,18 +2,14 @@
 
 namespace BestCom
 {
-	void simpleFunc(void)
-	{
-		std::cout << "BestCom이 정의한 함수" << std::endl;
-	}
+	void simpleFunc(void);
+
 }
 
 namespace ProgCom
 {
-	void simpleFunc(void)
-	{
-		std::cout << "ProgCom이 정의한 함수" << std::endl;
-	}
+	void simpleFunc(void);
+
 }
 
 int main(void)
@@ -21,4 +17,14 @@ int main(void)
 	BestCom::simpleFunc();
 	ProgCom::simpleFunc();
 	return 0;
+}
+
+void BestCom::simpleFunc(void)
+{
+	std::cout << "BestCom이 정의한 함수" << std::endl;
+}
+
+void ProgCom::simpleFunc(void)
+{
+	std::cout << "ProgCom이 정의한 함수" << std::endl;
 }
