86d5c17dc5eb2cdce46307c7c8349a51ad889d43
JH Baek <zirubak@gmail.com>
2014-06-29 21:15:13 -0700

    question 01-4 page 43

---
+++ def.h
@@ -0,0 +1,32 @@
+/*
+ * def.h
+ *
+ *  Created on: Jun 29, 2014
+ *      Author: jh
+ */
+
+#ifndef DEF_H_
+#define DEF_H_
+
+
+
+namespace BestCom
+{
+	void simpleFunc(void);
+
+}
+
+namespace BestCom
+{
+	void goodFunc(void);
+
+}
+
+namespace ProgCom
+{
+	void simpleFunc(void);
+
+}
+
+
+#endif /* DEF_H_ */
--- example.cpp
+++ example.cpp
@@ -1,22 +1,5 @@
 #include <iostream>
-
-namespace BestCom
-{
-	void simpleFunc(void);
-
-}
-
-namespace BestCom
-{
-	void goodFunc(void);
-
-}
-
-namespace ProgCom
-{
-	void simpleFunc(void);
-
-}
+#include "def.h"
 
 int main(void)
 {
@@ -25,18 +8,4 @@ int main(void)
 	return 0;
 }
 
-void BestCom::simpleFunc(void)
-{
-	std::cout << "BestCom이 정의한 함수" << std::endl;
-	goodFunc();
-}
 
-void BestCom::goodFunc(void)
-{
-	std::cout << "good 함수" << std::endl;
-}
-
-void ProgCom::simpleFunc(void)
-{
-	std::cout << "ProgCom이 정의한 함수" << std::endl;
-}
