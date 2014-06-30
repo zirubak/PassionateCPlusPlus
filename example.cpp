96c46dd8cd682041846ce16968974ddcd5390ef6
JH Baek <zirubak@gmail.com>
2014-06-29 22:57:52 -0700

    함수 내에서 전역 변수 접근하는 방법
    전역변수에 접근 하려면 scope resolution operator 를 사용하면 된다.
    
    Example)
    ::val += 7;

--- example.cpp
+++ example.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 
-//using std::cout;
-//using std::endl;
-//using std::cin;
+int val = 100;
 
-using namespace std;
-
+int simple(void)
+{
+	int val = 20;
+	val += 3; // 지역변수 val의 값 3 증가
+	::val += 7; // 전역변수 val의 값 7 증가
+	return val;
+}
 
 int main(void)
 {
-	int num = 10;
-	cout << num;
+	std::cout << val << std::endl;
+	std::cout << simple() << std::endl;
+	std::cout << val << std::endl;
 	return 0;
 }
