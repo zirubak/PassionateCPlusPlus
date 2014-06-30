16a310e0df9fc8d92822cf2718f97eeeba957f7a
JH Baek <zirubak@gmail.com>
2014-06-29 18:51:26 -0700

    simple adder page 17
    c++에서는 데이터의 입력도 출력과 마찬가지로 별도의 포멧을 지정해 줄 필요가 없다.
    그 대상이 문자열이라고 해도 마찬가지이다.

--- example.cpp
+++ example.cpp
@@ -10,12 +10,17 @@
 
 int main(void)
 {
-	int num = 20;
+	int val1;
+	std::cout << "첫 번째 숫자 입력: ";
+	std::cin >> val1;
 
-	std::cout<<"Hello World!"<<std::endl;
-	std::cout<<"Hello "<<"World!"<<std::endl;
-	std::cout<<num<<' '<<'A';
-	std::cout<<' '<<3.14<<std::endl;
+	int val2;
+	std::cout << "두 번째 숫자 입력: ";
+	std::cin >> val2;
+
+	int result = val1 + val2;
+	std::cout << "덧샘 결과: " << result << std::endl;
+
 	return 0;
 }
 
