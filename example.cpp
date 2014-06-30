03878daacdfaa778e81a351a4f5327ffed6ace0b
JH Baek <zirubak@gmail.com>
2014-06-29 20:39:44 -0700

    inline function page 34
    inline 함수는 프로그램 코드 라인으로 들어가 버린 함수. 
    속도가 빠른 장점이 있음.
    
    매크로의 장점은 자료형에 의존적이지 않지만 inline함수는 의존적이다.
    
    c++의 템플릿이라는 것을 이용하면 매크로 함수와 마찬가지로 자료형에 의존적이지
    않은 함수가 완성된다.

--- example.cpp
+++ example.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 
-int BoxVolume(int lenth, int width = 1, int height = 1);
+inline int square(int x)
+{
+	return x * x;
+}
 
 int main(void)
 {
-	std::cout << "[3,3,3]" << BoxVolume(3,3,3) << std::endl;
-	std::cout << "[5,5,D]" << BoxVolume(5,5) << std::endl;
-	std::cout << "[7,D,D]" << BoxVolume(7) << std::endl;
-//	std::cout << "[D,D,D]" << BoxVolume() << std::endl;
-	return 0;
-}
+	std::cout << square(5) << std::endl;
 
-int BoxVolume(int lenth, int width, int height)
-{
-	return lenth*width*height;
+	return 0;
 }
