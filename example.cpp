919d69db54f844e74873e02e69c57a6a92c47f9e
JH Baek <zirubak@gmail.com>
2014-06-30 21:29:09 -0700

    refernce pointer page 73
    포인터 변수도 변수이기 때문에 참조자의 선언이 가능하다.

--- example.cpp
+++ example.cpp
@@ -1,5 +1,4 @@
-//참조자는 무조건 선언과 동시에 변수를 참조하도록 해야 한다.
-//그런데 여기서 말하는 변수의 범위에는 배열의 요소도 포함된다.
+//포인터 변수도 변수이기 때문에 참조자의 선언이 가능하다.
 
 #include <iostream>
 
@@ -7,14 +6,17 @@ using namespace std;
 
 int main(void)
 {
-	int arr[3] = {1, 3, 5};
-	int &ref1 = arr[0];
-	int &ref2 = arr[1];
-	int &ref3 = arr[2];
+	int num = 12;
+	int *ptr = &num;
+	int **dptr = &ptr;
 
-	cout << ref1 << endl;
-	cout << ref2 << endl;
-	cout << ref3 << endl;
+	int &ref = num;
+	int *(&pref) = ptr;
+	int **(&dpref) = dptr;
+
+	cout << ref << endl;
+	cout << *pref << endl;
+	cout << **dpref << endl;
 
 	return 0;
 }
