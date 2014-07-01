a59c1ac57762e108512e6c0831dd81084673d9c5
JH Baek <zirubak@gmail.com>
2014-06-30 21:24:11 -0700

    reference array element page 72
    참조자는 무조건 선언과 동시에 변수를 참조하도록 해야 한다.
    그런데 여기서 말하는 변수의 범위에는 배열의 요소도 포함된다.

--- example.cpp
+++ example.cpp
@@ -1,25 +1,20 @@
+//참조자는 무조건 선언과 동시에 변수를 참조하도록 해야 한다.
+//그런데 여기서 말하는 변수의 범위에는 배열의 요소도 포함된다.
+
 #include <iostream>
 
 using namespace std;
 
 int main(void)
 {
-	int num1 = 1020;
-	int &num2 = num1;
+	int arr[3] = {1, 3, 5};
+	int &ref1 = arr[0];
+	int &ref2 = arr[1];
+	int &ref3 = arr[2];
 
-	num2 = 3047;
-	cout << "VAL: " << num1 << endl;
-	cout << "REF: " << num2 << endl;
-
-	cout << "VAL: " << &num1 << endl;
-	cout << "REF: " << &num2 << endl;
+	cout << ref1 << endl;
+	cout << ref2 << endl;
+	cout << ref3 << endl;
 
 	return 0;
-
 }
-
-//## result
-//VAL: 3047
-//REF: 3047
-//VAL: 0x7fff0eb7e6c4
-//REF: 0x7fff0eb7e6c4	num1과 num2의 주소값이 동일하다 이 결과에 주목해야 한다.
