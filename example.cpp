4df11aea9730e6d74658772907562f869d9684e6
JH Baek <zirubak@gmail.com>
2014-07-02 10:14:16 -0700

    question 03-1 page 114
    구조체내에 함수 정의하기

--- example.cpp
+++ example.cpp
@@ -1,89 +1,39 @@
 #include <iostream>
-
 using namespace std;
 
-#define ID_LEN 20
-#define MAX_SPD 200
-#define FUEL_STEP 2
-#define ACC_STEP 10
-#define BRK_STEP 10
-
-struct Car
+struct Point
 {
-	char gamerID[ID_LEN];
-	int fuelGauge;
-	int curSpeed;
+	int xpos;
+	int ypos;
 
-	void ShowCarState()
+	void Move(int x, int y)
 	{
-		cout << "소유자ID: " << gamerID << endl;
-		cout << "연료량: " << fuelGauge << "%" << endl;
-		cout << "현재속도: " << curSpeed << "km/s" << endl << endl;
+		xpos += x;
+		ypos += y;
 	}
 
-	void Accel()
+	void AddPoint(const Point &pos)
 	{
-		if(fuelGauge <= 0)
-			return;
-		else
-			fuelGauge -= FUEL_STEP;
-
-		if(curSpeed + ACC_STEP >= MAX_SPD)
-		{
-			curSpeed = MAX_SPD;
-			return;
-		}
-
-		curSpeed += ACC_STEP;
+		xpos += pos.xpos;
+		ypos += pos.ypos;
 	}
 
-	void Break()
+	void ShowPosition(void)
 	{
-		if(curSpeed < BRK_STEP)
-		{
-			curSpeed = 0;
-			return;
-		}
-
-		curSpeed -= BRK_STEP;
+		cout << "[" << xpos << "],[" << ypos << "]" << endl;
 	}
 };
 
-
 int main(void)
 {
-	Car run99 = {"run99", 100, 0};
-	run99.Accel();
-	run99.Accel();
-	run99.ShowCarState();
-	run99.Break();
-	run99.ShowCarState();
+	Point pos1 = {12,4};
+	Point pos2 = {20,30};
 
-	Car sped77 = {"sped77", 100, 0};
-	sped77.Accel();
-	sped77.Break();
-	sped77.ShowCarState();
+	pos1.Move(-7, 10);
+	pos1.ShowPosition();
+
+	pos1.AddPoint(pos2);
+	pos1.ShowPosition();
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
