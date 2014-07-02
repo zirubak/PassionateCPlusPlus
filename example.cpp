1196554cc6a8876a14d9fa736efe33726feaf94c
JH Baek <zirubak@gmail.com>
2014-07-02 09:51:11 -0700

    racing car function add in struct page 112
    C++에서는 struct 안에 함수를 넣는 것을 허용한다. 
    함수가 구조체 내에 삽입되면서 구조체 내에 선언된 변수에 직접접근이 가능해 졌기 
    때문이다.

--- example.cpp
+++ example.cpp
@@ -13,55 +13,56 @@ struct Car
 	char gamerID[ID_LEN];
 	int fuelGauge;
 	int curSpeed;
+
+	void ShowCarState()
+	{
+		cout << "소유자ID: " << gamerID << endl;
+		cout << "연료량: " << fuelGauge << "%" << endl;
+		cout << "현재속도: " << curSpeed << "km/s" << endl << endl;
+	}
+
+	void Accel()
+	{
+		if(fuelGauge <= 0)
+			return;
+		else
+			fuelGauge -= FUEL_STEP;
+
+		if(curSpeed + ACC_STEP >= MAX_SPD)
+		{
+			curSpeed = MAX_SPD;
+			return;
+		}
+
+		curSpeed += ACC_STEP;
+	}
+
+	void Break()
+	{
+		if(curSpeed < BRK_STEP)
+		{
+			curSpeed = 0;
+			return;
+		}
+
+		curSpeed -= BRK_STEP;
+	}
 };
 
-void ShowCarState(const Car &car)
-{
-	cout << "소유자ID: " << car.gamerID << endl;
-	cout << "연료량: " << car.fuelGauge << "%" << endl;
-	cout << "현재속도: " << car.curSpeed << "km/s" << endl << endl;
-}
-
-void Accel(Car &car)
-{
-	if(car.fuelGauge <= 0)
-		return;
-	else
-		car.fuelGauge -= FUEL_STEP;
-
-	if(car.curSpeed + ACC_STEP >= MAX_SPD)
-	{
-		car.curSpeed = MAX_SPD;
-		return;
-	}
-
-	car.curSpeed += ACC_STEP;
-}
-
-void Break(Car &car)
-{
-	if(car.curSpeed < BRK_STEP)
-	{
-		car.curSpeed = 0;
-		return;
-	}
-
-	car.curSpeed -= BRK_STEP;
-}
 
 int main(void)
 {
 	Car run99 = {"run99", 100, 0};
-	Accel(run99);
-	Accel(run99);
-	ShowCarState(run99);
-	Break(run99);
-	ShowCarState(run99);
+	run99.Accel();
+	run99.Accel();
+	run99.ShowCarState();
+	run99.Break();
+	run99.ShowCarState();
 
 	Car sped77 = {"sped77", 100, 0};
-	Accel(sped77);
-	Break(sped77);
-	ShowCarState(sped77);
+	sped77.Accel();
+	sped77.Break();
+	sped77.ShowCarState();
 
 	return 0;
 }
