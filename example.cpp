63dce85d1a94aa9652222a2fa8fdc4a27f0490f0
JH Baek <zirubak@gmail.com>
2014-07-02 00:17:18 -0700

    Question 02-3 page 95
    중요함.. 다시 한번 잘 생각해 보고 reference에 대해서 확실하게 이해야하자.

--- example.cpp
+++ example.cpp
@@ -1,26 +1,47 @@
-//new는 malloc을 대신하는 키워드이고
-//delete는 free 함수를 대신하는 키워드이다.
+//Question 02-3
+//page 95
 
 #include <iostream>
-#include <string.h>
-//#include <stdlib.h>
 
 using namespace std;
 
-char* MakeStrAdr(int len)
+typedef struct _Point{
+	int xpos;
+	int ypos;
+} Point;
+
+//위의 구조체를 기반으로 두 점의 합을 계산하는 함수를 다음의 형태로 정의하고
+//덧샘의 결과는 함수의 반환을 통해서 얻게 한다.
+
+Point& PntAdder(const Point &p1, const Point &p2)
 {
-//	char* str = (char*)malloc(sizeof(char) * len);
-	char* str = new char[len];
-	return str;
+	Point *rtn = new Point;
+
+	rtn->xpos = p1.xpos + p2.xpos;
+	rtn->ypos = p1.ypos + p2.ypos;
+
+	return *rtn;
 }
 
 int main(void)
 {
-	char* str = MakeStrAdr(20);
-	strcpy(str, "I am so happy~");
-	cout << str << endl;
-//	free(str);
-	delete []str;
+//	Point *rtn = new Point;
+	Point *ptr1 = new Point;
+	Point *ptr2 = new Point;
+
+	ptr1->xpos = 3;
+	ptr1->ypos = 30;
+
+	ptr2->xpos = 70;
+	ptr2->ypos = 7;
+
+	Point &rtn = PntAdder(*ptr1, *ptr2);
+
+	cout << "[" << rtn.xpos << "," << rtn.ypos << "]" << endl;
+
+	delete ptr1;
+	delete ptr2;
+	delete &rtn;	//이건 좀더 생각해 보자.. 왜 이렇게 해야 하는지.
 
 	return 0;
 }
