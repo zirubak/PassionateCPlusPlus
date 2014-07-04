a3f7792ca66db0bf8db25d9157e528b59ec2a8be
JH Baek <zirubak@gmail.com>
2014-07-04 15:05:28 -0700

    사과 장사 시뮬레이션 page 142
    객체지향 프로그래밍은 현실에 존재하는 사물과 대상, 그리고 그에 따른 행동을 있는 
    그대로 실체화 시키는 형태의 프로그램이다. 
    
    객체를 이루는 것은 데이터와 기능이다. 
    Object = Data (Value) + Action (Fuction)
    
    객체는 하나 이상의 상태 정보(데이터)와 하나 이상의 행동(기능)으로 구성된다. 
    
    상수는 선언과 동시에 초기화 되어야 한다. 
    
    #클래스 기반의 두가지 객체 생성 방법
    - 일반적인 변수의 선언 방식( in Stack )
    ClassName objName;
    
    -동적 할당 방식( 힙 할당 방식 )
    ClassName *ptrObj = new ClassName;
    
    #객체간의 대화 방법(messae passing 방법)
    하나의 객체가 다른 하나의 객체에게 메세지를 전달하는 방법은 함수 호출을 기반으로 한다.
    그래서 객체지향에서는 이러한 형태의 함수호출을 가리켜 메세지 전달(Message passing)
    이라고 한다.

--- main.cpp
+++ main.cpp
@@ -6,19 +6,79 @@
  */
 
 #include <iostream>
-#include "Printer.h"
-
 using namespace std;
+
+class FruitSeller
+{
+private:
+	int APPLE_PRICE;
+	int numOfApples;
+	int myMoney;
+
+public:
+	void InitMembers(int price, int num, int money)
+	{
+		APPLE_PRICE = price;
+		numOfApples = num;
+		myMoney = money;
+	}
+	int SaleApples(int money)
+	{
+		int num = money / APPLE_PRICE;
+		numOfApples -= num;
+		myMoney += money;
+		return num;
+	}
+	void ShowSalesResult()
+	{
+		cout << "남은 사과: " << numOfApples <<endl;
+		cout << "현재 수익: " << myMoney <<endl;
+	}
+};
+
+class FruitBuyer
+{
+	int myMoney;	//private
+	int numOfApples;	//private
+
+public:
+	void InitMembers(int money)
+	{
+		myMoney = money;
+		numOfApples = 0;
+	}
+	void BuyApples(FruitSeller &seller, int money)
+	{
+		numOfApples += seller.SaleApples(money);
+		myMoney -= money;
+	}
+	void ShowBuyResult()
+	{
+		cout << "현재 잔액: " << myMoney << endl;
+		cout << "사과 개수: " << numOfApples << endl;
+	}
+};
 
 int main(void)
 {
-	Printer pnt;
-	pnt.SetString("Hello world!");
-	pnt.ShowString();
+	FruitSeller seller;
+	seller.InitMembers(1000, 20, 0);
 
-	pnt.SetString("I love C++.");
-	pnt.ShowString();
+	FruitBuyer buyer;
+	buyer.InitMembers(5000);
+	buyer.BuyApples(seller, 2000); // buy apples.
+
+	cout << "과일 판매자의 현황" << endl;
+	seller.ShowSalesResult();
+	cout << "과일 구매자의 현황" << endl;
+	buyer.ShowBuyResult();
+
 	return 0;
 }
+
+
+
+
+
 
 
