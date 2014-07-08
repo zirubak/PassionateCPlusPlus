2c7a468d8f39e36f93b0c5cb51f148d338c44524
JH Baek <zirubak@gmail.com>
2014-07-08 00:37:23 -0700

    question 04-3 #1 page 190

--- Ring.cpp
+++ Ring.cpp
@@ -15,10 +15,15 @@ private:
 	int xpos, ypos;
 
 public:
-	void Init(int x, int y)
+//	void Init(int x, int y)
+//	{
+//		xpos = x;
+//		ypos = y;
+//	}
+
+	Point(int x, int y):xpos(x), ypos(y)
 	{
-		xpos = x;
-		ypos = y;
+
 	}
 
 	void ShowPointInfo() const
@@ -36,10 +41,15 @@ private:
 
 public:
 	//반지름의 길이 정보를 저장
-	void init(int x, int y, int r)
+//	void init(int x, int y, int r)
+//	{
+//		radius = r;
+//		circleCenterPos.Init(x,y);
+//	}
+
+	Circle(int x, int y, int r):circleCenterPos(x , y), radius(r)
 	{
-		radius = r;
-		circleCenterPos.Init(x,y);
+
 	}
 
 	//반지름의 길이 정보를 출력
@@ -58,10 +68,16 @@ private:
 	Circle outCircle;
 
 public:
-	void Init(int inX, int inY, int inR, int outX, int outY, int outR)
+//	void Init(int inX, int inY, int inR, int outX, int outY, int outR)
+//	{
+//		inCircle.init(inX, inY, inR);
+//		outCircle.init(outX, outY, outR);
+//	}
+
+	Ring(int inX, int inY, int inR, int outX, int outY, int outR)
+		:inCircle(inX, inY, inR), outCircle(outX, outY, outR)
 	{
-		inCircle.init(inX, inY, inR);
-		outCircle.init(outX, outY, outR);
+
 	}
 
 	void ShowRingInfo() const
@@ -76,9 +92,9 @@ public:
 
 int main(void)
 {
-	Ring ring;
-	ring.Init(1,1,4,2,2,9);
+	Ring ring(1,1,4,2,2,9);
 	ring.ShowRingInfo();
+
 	return 0;
 }
 
