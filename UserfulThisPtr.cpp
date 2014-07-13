35c1b9049d49b13fce1b6375ff88478c123abbbd
JH Baek <zirubak@gmail.com>
2014-07-12 17:53:00 -0700

    this pointer의 활용
    page 198
    
    객체의 포인터를 가지고는 지역변수에 접근이 불가능 하다.
    따라서 이는 맴버 변수 num을 의미하는 것이다.
    
    이러한 특성을 활용하면 매개변수의 이름을 맴버변수의 이름과 달리하기 위해서 
    고민할 필요가 없다.

---
+++ UserfulThisPtr.cpp
@@ -0,0 +1,43 @@
+/*
+ * UserfulThisPtr.cpp
+ *
+ *  Created on: Jul 12, 2014
+ *      Author: jh
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class TwoNumber
+{
+private:
+	int num1;
+	int num2;
+
+public:
+	TwoNumber(int num1, int num2)
+	{
+		this->num1 = num1;
+		this->num2 = num2;
+	}
+
+//	TwoNumber(int num1, int num2)
+//		:num1(num1) , num2(num2)
+//	{
+//	}
+
+	void ShowTwoNumber()
+	{
+		cout << this->num1 << endl;
+		cout << this->num2 << endl;
+	}
+};
+
+int main(void)
+{
+	TwoNumber two(2,4);
+	two.ShowTwoNumber();
+
+	return 0;
+}
