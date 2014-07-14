cf2c898c0c13c7546b169cec2b5049bb4a67f215
JH Baek <zirubak@gmail.com>
2014-07-13 23:13:22 -0700

    control class or handler class
    page 275
    
    컨트롤 클래스는 기능의 처리를 실제로 담당하는 클래스이다.
    컨트롤 클래스는 기능 제공의 핵심이 되기 때문에 이 프로그램이 어떠한 기능을 제공하는지
    구체적으로 알 수 있다.

--- EmployeeManager1.cpp
+++ EmployeeManager1.cpp
@@ -27,3 +27,45 @@ public:
 		cout << "salary: " << GetPay() << endl;
 	}
 };
+
+class EmployeeHander
+{
+private:
+	PermanentWorker* empList[50];
+	int empNum;
+
+public:
+	EmployeeHander() : empNum(0)
+	{}
+
+	void AddEmployee(PermanentWorker* emp)
+	{
+		empList[empNum++] = emp;
+	}
+
+	void ShowAllSalaryInfo() const
+	{
+		for(int i = 0; i < empNum; i++)
+		{
+			empList[i]->ShowSalaryInfo();
+		}
+	}
+
+	void ShowTotalSalary() const
+	{
+		int sum = 0;
+		for(int i = 0; i < empNum; i++)
+		{
+			sum += empList[i]->GetPay();
+		}
+		cout << "salary sun: " << sum << endl;
+	}
+
+	~EmployeeHander()
+	{
+		for(int i = 0; i < empNum; i++)
+		{
+			delete empList[i];
+		}
+	}
+};
