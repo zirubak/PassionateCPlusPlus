f2adbdfde92676d422c23b2735b6cec13c6dbf76
JH Baek <zirubak@gmail.com>
2014-07-13 23:34:49 -0700

    add main()
    page 275
    
    모든 소프트웨어의 설계에 있어서 중요시하는 것 중 하나는 다음과 같다.
    
    #요구 사항의 변경에 대응하는 프로그램의 유연성
    #기능의 추가에 따른 프로그램의 확장성

--- EmployeeManager1.cpp
+++ EmployeeManager1.cpp
@@ -69,3 +69,22 @@ public:
 		}
 	}
 };
+
+int main(void)
+{
+	//직원 관리를 목적으로 설계된 컨트롤 클래스의 객체 생성
+	EmployeeHander handler;
+
+	//직원 등록
+	handler.AddEmployee(new PermanentWorker("KIM", 1000));
+	handler.AddEmployee(new PermanentWorker("Lee", 1500));
+	handler.AddEmployee(new PermanentWorker("JUN", 2000));
+
+	//이번 달에 지불해야 할 급여의 정보
+	handler.ShowAllSalaryInfo();
+
+	//이번 달에 지불해야 할 급여의 총합
+	handler.ShowTotalSalary();
+
+	return 0;
+}
