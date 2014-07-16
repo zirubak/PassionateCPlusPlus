4070f5cb24ca14c46111ff1e219a68776e722175
JH Baek <zirubak@gmail.com>
2014-07-15 22:06:11 -0700

    유도 클래스 객체의 소멸 과정
    page 291
    
    유도 클래스의 객체가 소멸될 때에는 유도 클래스의 소멸자가 실행되고 난 다음에
    기초 클래스의 소멸자가 실행된다. 
    
    스택에 생성된 객체의 소멸 순서는 생성 순서와 반대이다.
    
    중요한 사실은 기초 클래스의 소멸자도 유도 클래스의 소멸자도 호출 된다는 사실이다.
    
    "생성자에서 동적 할당한 메모리 공간은 소멸자에서 해제한다."

--- DrivCreOrder.cpp
+++ DrivCreOrder.cpp
@@ -7,19 +7,14 @@ private:
 	int baseNum;
 
 public:
-	SoBase() : baseNum(20)
-	{
-		cout << "SoBase()" << endl;
-	}
-
 	SoBase(int n) : baseNum(n)
 	{
 		cout << "SoBase(int n)" << endl;
 	}
 
-	void ShowBaseData()
+	~SoBase()
 	{
-		cout << baseNum << endl;
+		cout << "~SoBase(): " << baseNum << endl;
 	}
 };
 
@@ -29,43 +24,21 @@ private:
 	int derivNum;
 
 public:
-	SoDerived() : derivNum(30)
-	{
-		cout << "SoDerived()" << endl;
-	}
-
-	SoDerived(int n) : derivNum(n)
-	{
-		cout << "SoDerived(int n)" << endl;
-	}
-
-	SoDerived(int n1, int n2) : SoBase(n1), derivNum(n2)
+	SoDerived(int n) : SoBase(n), derivNum(n)
 	{
 		cout << "SoDerived(int n1, int n2)" << endl;
 	}
 
-	void ShowDerivData()
+	~SoDerived()
 	{
-		ShowBaseData();
-		cout << derivNum << endl;
+		cout << "~SoDerived() : " <<  derivNum << endl;
 	}
 };
 
 int main(void)
 {
-	cout << "case1...." << endl;
-	SoDerived dr1;
-	dr1.ShowDerivData();
-
-	cout << "---------------------" << endl;
-	cout << "case2...." << endl;
-	SoDerived dr2(12);
-	dr2.ShowDerivData();
-
-	cout << "---------------------" << endl;
-	cout << "case3...." << endl;
-	SoDerived dr3(23, 24);
-	dr3.ShowDerivData();
+	SoDerived drv1(15);
+	SoDerived drv2(17);
 
 	return 0;
 }
