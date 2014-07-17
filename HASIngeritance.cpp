6cd1d6c1cc1c576e4396001225ca010c81f5112a
JH Baek <zirubak@gmail.com>
2014-07-16 22:37:25 -0700

    상속이 아닌 다른 방식으로 표현하기
    page 309
    
    ****상속은 IS-A 관계의 표현에 매우 적절하다. 그리고 경우에 따라서는 HAS-A 관계의 표
    현에도(소유관계의 표현에도) 사용될 수 있으나 이는 프로그램의 변경에 많은 제약을 가져다
    줄 수 있다. 
    
    **상속으로 표현한 HAS-A 관계는 오히려 코드 양을 더 많이 늘리고 확장성이 떨어진다.
    
    **상속으로 묶인 두개의 클래스는 강한 연관성을 뛴다.

--- HASIngeritance.cpp
+++ HASIngeritance.cpp
@@ -20,21 +20,40 @@ public:
 	}
 };
 
-class Police : public Gun
+class Police
 {
 private:
 	int handcuffs;	//소유한 수갑의 수
+	Gun * pistol;	//소유하고 있는 권총
 
 public:
 	Police(int bnum, int bcuff)
-		:Gun(bnum), handcuffs(bcuff)
+		:handcuffs(bcuff)
 	{
+		if(bnum > 0)
+			pistol = new Gun(bnum);
+		else
+			pistol = NULL;
 	}
 
 	void PutHandcuff()
 	{
 		cout <<"SNAP!" << endl;
 		handcuffs--;
+	}
+
+	void Shot()
+	{
+		if(pistol == NULL)
+			cout << "Hut BBANG!" << endl;
+		else
+			pistol->Shot();
+	}
+
+	~Police()
+	{
+		if(pistol != NULL)
+			delete pistol;
 	}
 };
 
@@ -43,6 +62,11 @@ int main(void)
 	Police pman(5, 3); 	//총알 5, 수갑 3
 	pman.Shot();
 	pman.PutHandcuff();
+
+	Police pman2(0, 3);
+	pman2.Shot();
+	pman2.PutHandcuff();
+
 	return 0;
 }
 
