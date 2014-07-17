e8e489784df5b5f456780ff1dc7fda2d7f552981
JH Baek <zirubak@gmail.com>
2014-07-17 08:37:12 -0700

    Banking System ver 0.5
    Control class를 구현해 보자.
    객체지향에는 "전역"이라는 개념이 존재하지 않는다. 
    비롯 C++에는 전역함수과 전역 변수의 선언이 허용하고 있지만
    이는 객체지향 프로그래밍을 위한 것은 아니니 가급적 사용하지 않는 것이 좋다.
    
    *control class의 특징
    -프로그램 전체의 기능을 담당한다. 따라서 기능적 성격이 강한 클래스이다.
    -컨트롤 클래스만 봐도 프로그램의 전체 기능과 흐름을 파악할 수 있다.
    
    *Entity class의 특징
    -데이터적 성격이 강하다. 따라서 파일 및 데이터 베이스에 저장되는 데이터를 
    소유하고 있다.
    -프로그램의 기능을 파악하는데 도움을 주지는 못한다.
    -그러나 프로그램상에서 관리되는 데이터의 종류를 파악하는 데는 도움이 된다.

--- example.cpp
+++ example.cpp
@@ -1,18 +1,14 @@
-//Banking System ver 0.4
+//Banking System ver 0.5
 
-//const선언을 추가해서 코드의 안정성을 높여 보자
+//Control class를 구현해 보자.
+//객체지향에는 "전역"이라는 개념이 존재하지 않는다. 비롯 C++에는 전역함수과 전역 변수의 선언이 허용하고 있지만
+//이는 객체지향 프로그래밍을 위한 것은 아니니 가급적 사용하지 않는 것이 좋다.
 
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 const int NAME_LEN = 20;
-
-void ShowMenu(void);	//메뉴 출력
-void MakeAccount(void);	//계좌개설을 위한 함수
-void DepositMoney(void);	//입금
-void WithdrawMoney(void);	//출금
-void ShowAllAccInfo(void);	//잔액 조회
 
 enum{
 	MAKE = 1,
@@ -22,6 +18,7 @@ enum{
 	EXIT
 };
 
+//Entity class
 class Account{
 private:
 	int accID;	//계좌번호
@@ -30,96 +27,95 @@ private:
 	char* cusName;	//고객이름
 
 public:
-	Account(int ID, int money, char* name)
-		:accID(ID), balance(money)
-	{
-		cusName = new char[strlen(name)+1];
-		strcpy(cusName, name);
-	}
-
+	//가독성을 높이기 위해, 클래스 내에는 멤버함수의 선언만 두고 정의는 클래스
+	//외부로 빼 내었다
+	Account(int ID, int money, char* name);
 	//deep copy constructor
-	Account(const Account& obj) : accID(obj.accID), balance(obj.balance)
-	{
-		cusName = new char[strlen(obj.cusName)+1];
-		strcpy(cusName, obj.cusName);
-	}
+	Account(const Account& obj);
+	~Account();
 
-	~Account()
-	{
-		delete []cusName;
-	}
-
-	int GetAccID() const
-	{
-		return accID;
-	}
-
-	void Deposit(int money)
-	{
-		balance += money;
-	}
-
-	int Withdraw(int money)
-	{
-		if(balance < money)
-			return 0;
-
-		balance -= money;
-		return money;
-	}
-
-	void ShowAccInfo() const
-	{
-		cout << "계좌 ID: " << accID << endl;
-		cout << "이 름" << cusName << endl;
-		cout << "잔 액" << balance << endl << endl;
-	}
+	int GetAccID() const;
+	void Deposit(int money);
+	int Withdraw(int money);
+	void ShowAccInfo() const;
 };
 
-Account* accArr[100];	//Account 저장을 위한 배열
-int accNum = 0;	//저장된 Account 수
 
-int main(void)
+Account::Account(int ID, int money, char* name)
+	:accID(ID), balance(money)
 {
-	int choice;
-
-	while(1)
-	{
-		ShowMenu();
-		cout << "선택: ";
-		cin >> choice;
-		cout << endl;
-
-		switch(choice)
-		{
-		case MAKE:
-			MakeAccount();
-			break;
-
-		case DEPOSIT:
-			DepositMoney();
-			break;
-
-		case WITHDRAW:
-			WithdrawMoney();
-			break;
-
-		case INQUIRE:
-			ShowAllAccInfo();
-			break;
-
-		case EXIT:
-			return 0;
-		default:
-			cout << "Illegal selection." << endl;
-		}
-	}
-
-	return 0;
-
+	cusName = new char[strlen(name)+1];
+	strcpy(cusName, name);
 }
 
-void ShowMenu(void)	//메뉴 출력
+//deep copy constructor
+Account::Account(const Account& obj) : accID(obj.accID), balance(obj.balance)
+{
+	cusName = new char[strlen(obj.cusName)+1];
+	strcpy(cusName, obj.cusName);
+}
+
+int Account::GetAccID() const
+{
+	return accID;
+}
+
+void Account::Deposit(int money)
+{
+	balance += money;
+}
+
+int Account::Withdraw(int money)
+{
+	if(balance < money)
+		return 0;
+
+	balance -= money;
+	return money;
+}
+
+void Account::ShowAccInfo() const
+{
+	cout << "계좌 ID: " << accID << endl;
+	cout << "이 름" << cusName << endl;
+	cout << "잔 액" << balance << endl << endl;
+}
+
+Account::~Account()
+{
+	delete []cusName;
+}
+
+
+
+//Control Class
+
+class AccountHandler
+{
+private:
+	Account* accArr[100];	//Account 저장을 위한 배열
+	int accNum;	//저장된 Account 수
+
+public:
+	AccountHandler();
+	~AccountHandler();
+	void ShowMenu(void) const;	//메뉴 출력
+	void MakeAccount(void);	//계좌개설을 위한 함수
+	void DepositMoney(void);	//입금
+	void WithdrawMoney(void);	//출금
+	void ShowAllAccInfo(void);	//잔액 조회
+};
+
+AccountHandler::AccountHandler()
+	:accNum(0)
+{
+}
+
+AccountHandler::~AccountHandler()
+{
+}
+
+void AccountHandler::ShowMenu(void) const	//메뉴 출력
 {
 	cout << "-----Menu-----" << endl;
 	cout << "1. 계좌 개설" << endl;
@@ -129,7 +125,7 @@ void ShowMenu(void)	//메뉴 출력
 	cout << "5. 프로그램 종료" << endl;
 }
 
-void MakeAccount(void)	//계좌개설을 위한 함수
+void AccountHandler::MakeAccount(void)	//계좌개설을 위한 함수
 {
 	int id;
 	char name[NAME_LEN];
@@ -147,7 +143,7 @@ void MakeAccount(void)	//계좌개설을 위한 함수
 	accArr[accNum++] = new Account(id, balance, name);
 }
 
-void DepositMoney(void)	//입금
+void AccountHandler::DepositMoney(void)	//입금
 {
 	int money;
 	int id;
@@ -169,7 +165,7 @@ void DepositMoney(void)	//입금
 	cout << "유효하지 않은 ID 입니다. " << endl << endl;
 }
 
-void WithdrawMoney(void)	//출금
+void AccountHandler::WithdrawMoney(void)	//출금
 {
 	int money;
 	int id;
@@ -197,11 +193,52 @@ void WithdrawMoney(void)	//출금
 	cout << "유효하지 않은 ID 입니다." << endl << endl;
 }
 
-void ShowAllAccInfo(void)	//잔액 조회
+void AccountHandler::ShowAllAccInfo(void)	//잔액 조회
 {
 	for(int i=0; i < accNum; i++)
 	{
 		accArr[i]->ShowAccInfo();
 		cout << endl;
 	}
+}
+
+
+int main(void)
+{
+	AccountHandler manager;
+	int choice;
+
+	while(1)
+	{
+		manager.ShowMenu();
+		cout << "선택: ";
+		cin >> choice;
+		cout << endl;
+
+		switch(choice)
+		{
+		case MAKE:
+			manager.MakeAccount();
+			break;
+
+		case DEPOSIT:
+			manager.DepositMoney();
+			break;
+
+		case WITHDRAW:
+			manager.WithdrawMoney();
+			break;
+
+		case INQUIRE:
+			manager.ShowAllAccInfo();
+			break;
+
+		case EXIT:
+			return 0;
+		default:
+			cout << "Illegal selection." << endl;
+		}
+	}
+
+	return 0;
 }
