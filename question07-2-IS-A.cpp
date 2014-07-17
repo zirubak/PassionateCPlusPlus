aae6a80a63fabb6fed0049ba6fda80f9f46e7fe7
JH Baek <zirubak@gmail.com>
2014-07-16 23:50:24 -0700

    question 2 in 07-2
    page 313

--- question07-2-IS-A.cpp
+++ question07-2-IS-A.cpp
@@ -1,41 +1,81 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
-class Rectagle
+class Book
 {
 private:
-	int width, height;
+	char* title;	//책의 제목
+	char* isbn;	//국제표준도서번호
+	int price; 	//책의 가격
 
 public:
-	Rectagle(int myWidth, int myHeight)
-		:width(myWidth), height(myHeight)
+	Book(char* newTitle, char* newISBN, int newPrice)
+		:price(newPrice)
 	{
+		title = new char[strlen(newTitle)+1];
+		isbn = new char[strlen(newISBN)+1];
+		strcpy(title, newTitle);
+		strcpy(isbn, newISBN);
 	}
 
-	void ShowAreaInfo()
+	void ShowBookInfo()
 	{
-		int area = width * height;
-		cout << "면적: " << area << endl;
+		cout << "Title: " << title << endl;
+		cout << "ISBN: " << isbn << endl;
+		cout << "Price: " << price << endl;
+	}
+
+	~Book()
+	{
+		delete []title;
+		delete []isbn;
 	}
 };
 
-class Square : public Rectagle
+class EBook : public Book
 {
 private:
+	char* DRMKey;	//보안키
 
 public:
-	Square(int myWidth): Rectagle(myWidth,myWidth)
+	EBook(char* newTitle, char* newISBN, int newPrice, char* NewDRMKey)
+		:Book(newTitle, newISBN, newPrice)
 	{
+		DRMKey = new char[strlen(NewDRMKey)+1];
+		strcpy(DRMKey, NewDRMKey);
+	}
+
+	void ShowEbbokInfo()
+	{
+		ShowBookInfo();
+		cout << "인증키: " << DRMKey << endl;
+	}
+
+	~EBook()
+	{
+		delete []DRMKey;
 	}
 };
 
 int main(void)
 {
-	Rectagle rec(4,3);
-	rec.ShowAreaInfo();
-
-	Square sqr(7);
-	sqr.ShowAreaInfo();
-
+	Book book("Good C++", "1231-12311-12344", 200);
+	book.ShowBookInfo();
+	cout << endl;
+	EBook ebook("Good C++ ebook", "234-553-2341", 100, "sdfswe213fs");
+	ebook.ShowEbbokInfo();
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
